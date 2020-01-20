82bc1fd703591ec094a65d3d6df9a5235286fb20
Taran0 <49963209+Taran0@users.noreply.github.com>
2020-01-20 23:59:19 +0100

    projects insert done

--- index.php
+++ index.php
@@ -38,17 +38,54 @@ require_once "include/db.php";
     </nav>
     <div class="container">
         <div class="row">
-            <h1 class="display-4 col d-inline">Projects</h1>
+            <h2 class="h3 col d-inline pt-4">Projects</h2>
             <a class="col text-info text-right align-text-bottom pt-4" href="" data-target="#newProjectModal" data-toggle="modal">
                 <i class="fas fa-plus-square fa-3x"></i>
             </a>
-
-            <!-- new project button -->
-            <!-- <button class="btn btn-secondary col  text-right align-text-bottom pt-4" data-toggle="modal" data-target="#myModal">
-                <i class="fas fa-plus-square fa-3x"></i>
-            </button> -->
-
         </div>
+<?php
+    if (isset($_POST['submit'])) {
+        if (!empty($_POST['projectName']) && !empty($_POST['projectStatus']) && !empty($_POST['projectStartTime']) && 
+        !empty($_POST['projectDeadline'])) {
+            $projectName = $_POST['projectName'];
+            $projectStatus = $_POST['projectStatus'];
+            $projectStartTime = $_POST['projectStartTime'];
+            $projectDeadline = $_POST['projectDeadline'];
+            $plannedWorkingTimeHours = $_POST['plannedWorkingTimeHours'];
+            $plannedWorkingTimeMinutes = $_POST['plannedWorkingTimeMinutes'];
+            $projectNote = $_POST['projectNote'];
+            
+            //planned working time calculation
+            $plannedWorkingTime = intval($plannedWorkingTimeHours) * 3600 + intval($plannedWorkingTimeMinutes) * 60;
+
+            $connection;
+            
+            $sql = "INSERT INTO projects(project_name, project_starttime, project_deadline, 
+            project_expectedtime, project_status, project_note)
+            VALUES(:pname, :pstart, :pdeadl, :pexptime, :pstatus, :pnote)";
+    
+            //connectingDB objekt, metódusa a prepare
+            $stmt = $connection->prepare($sql);
+            
+            //pdo-s value párosítása a változókkal
+            $stmt->bindValue(':pname', $projectName);
+            $stmt->bindValue(':pstart', $projectStartTime);
+            $stmt->bindValue(':pdeadl', $projectDeadline);
+            $stmt->bindValue(':pexptime', $plannedWorkingTime);
+            $stmt->bindValue(':pstatus', $projectStatus);
+            $stmt->bindValue(':pnote', $projectNote);   
+            
+            $execute = $stmt->execute();
+
+            if($execute){
+                echo "<span>Record has been added successfully</span>";
+            }
+    
+        } else {
+            echo "<span>please add the data</span>";
+        }
+    }
+?>
         <table class="table table-hover table-striped table-sm">
             <thead class="thead-dark">
                 <tr>
@@ -94,81 +131,82 @@ $i++;
     <!-- new project modal -->
     <div class="modal" id="newProjectModal">
         <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header bg-info">
-                  <h5 class="modal-title">Add new project</h5>
-                  <button class="close" data-dismiss="modal">&times;</button>
-                </div>
-                <div class="modal-body">
-                    <form action="">
-                        <div class="row">
-                            <div class="col-md-7">
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">Prject name</span>
+            <div class="modal-content">                
+                    <div class="modal-header bg-info">
+                      <h5 class="modal-title">Add new project</h5>
+                      <button class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="index.php" method="post">
+                            <div class="row">
+                                <div class="col-md-7">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">Project name</span>
+                                        </div>
+                                        <input name="projectName" type="text" class="form-control" required>
                                     </div>
-                                    <input name="projectName" type="text" class="form-control" placeholder="Project name">
+                                </div>
+                                <div class="col-md-5">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">Status</span>
+                                        </div>
+                                        <select name="projectStatus" class="form-control">
+                                            <option value="active">Active</option>
+                                            <option value="planned">Planned</option>
+                                            <option value="archived">Archived</option>
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-md-5">
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">Status</span>
+                            <div class="row pt-3">
+                                <div class="col">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">Project start</span>
+                                        </div>
+                                        <input name="projectStartTime" type="date" class="form-control" required>
                                     </div>
-                                    <select name="projectStatus" class="form-control">
-                                        <option value="volvo">Active</option>
-                                        <option value="saab">Planned</option>
-                                        <option value="opel">Archived</option>
-                                    </select>
                                 </div>
                             </div>
-                        </div>
-                        <div class="row pt-3">
-                            <div class="col">
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">Project start</span>
+                            <div class="row py-3">
+                                <div class="col">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">Deadline</span>
+                                        </div>
+                                        <input name="projectDeadline" type="date" class="form-control" required>
                                     </div>
-                                    <input name="projectStartTime" type="datetime-local" class="form-control">
                                 </div>
                             </div>
-                        </div>
-                        <div class="row py-3">
-                            <div class="col">
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">Deadline</span>
-                                    </div>
-                                    <input name="projectDeadline" type="datetime-local" class="form-control">
-                                </div>
-                            </div>
-                        </div>
 
-                        <div class="row">
-                            <div class="col-md-12 pb-3">
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">Planned working time</span>
+                            <div class="row">
+                                <div class="col-md-12 pb-3">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">Planned working time</span>
+                                        </div>
+                                        <input name="plannedWorkingTimeHours" class="form-control" type="number" placeholder="Hours">
+                                        <input name="plannedWorkingTimeMinutes" class="form-control" type="number" min="0" max="59" placeholder="Mins">
                                     </div>
-                                    <input class="form-control" type="number" placeholder="Hours">
-                                    <input class="form-control" type="number" min="0" max="59" placeholder="Mins">
                                 </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <textarea name="projectNote" class="form-control" placeholder="Project note"></textarea>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <textarea name="projectNote" class="form-control" placeholder="Project note"></textarea>
+                                    </div>
                                 </div>
                             </div>
+                            <input type="submit" name="submit" value="Create new project" class="btn btn-outline-danger btn-block">
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="form-group">
+                            <button class="btn btn-secondary" data-dismiss="modal">Close</button>
                         </div>
-                    </div>
-                </form>
-                <div class="modal-footer">
-                    <div class="form-group">
-                        <input type="submit" value="Create new project" class="btn btn-outline-danger">
-                    </div>
-                </div>
+                    </div>             
             </div>
         </div>
     </div>
