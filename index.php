8c0bed417ec27f7b0d0b3e7b805d80306ede8f10
Taran0 <49963209+Taran0@users.noreply.github.com>
2020-01-20 17:42:15 +0100

    project add form done

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ require_once "include/db.php";
     <title>Projects</title>
 </head>
 <body>
+    <!-- navbar -->
     <nav id="main-nav" class="navbar navbar-expand-sm navbar-dark bg-dark py-4">
         <div class="container">
             <a href="index.html" class="navbar-brand">
@@ -36,15 +37,18 @@ require_once "include/db.php";
         </div>
     </nav>
     <div class="container">
-    <div class="row">
-        <h1 class="display-4 col d-inline">Projects</h1>
-        <a class="text-secondary col text-right align-text-bottom pt-4" href="#">
-            <i class="fas fa-plus-square fa-3x"></i>
-        </a>
+        <div class="row">
+            <h1 class="display-4 col d-inline">Projects</h1>
+            <a class="col text-info text-right align-text-bottom pt-4" href="" data-target="#newProjectModal" data-toggle="modal">
+                <i class="fas fa-plus-square fa-3x"></i>
+            </a>
 
-    </div>
+            <!-- new project button -->
+            <!-- <button class="btn btn-secondary col  text-right align-text-bottom pt-4" data-toggle="modal" data-target="#myModal">
+                <i class="fas fa-plus-square fa-3x"></i>
+            </button> -->
 
-
+        </div>
         <table class="table table-hover table-striped table-sm">
             <thead class="thead-dark">
                 <tr>
@@ -73,7 +77,7 @@ while ($row = $stmt->fetch()) {
                                 <td class="align-middle"><?php echo $name; ?></td>
                                 <td class="align-middle"><?php echo $deadline; ?></td>
                                 <td><a href="tasks.php?id=<?php echo $id; ?>&projectname=<?php echo $name; ?>" class="btn btn-outline-danger btn-block btn-sm">Do</a></td>
-                                <td><a href="" class="btn btn-outline-primary btn-block btn-sm">Edit</a></td>
+                                <td><a href="" class="btn btn-outline-info btn-block btn-sm">Edit</a></td>
                             </tr>
                         <?php
 $i++;
@@ -85,6 +89,88 @@ $i++;
 
 
 
+    </div>
+
+    <!-- new project modal -->
+    <div class="modal" id="newProjectModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header bg-info">
+                  <h5 class="modal-title">Add new project</h5>
+                  <button class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <form action="">
+                        <div class="row">
+                            <div class="col-md-7">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">Prject name</span>
+                                    </div>
+                                    <input name="projectName" type="text" class="form-control" placeholder="Project name">
+                                </div>
+                            </div>
+                            <div class="col-md-5">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">Status</span>
+                                    </div>
+                                    <select name="projectStatus" class="form-control">
+                                        <option value="volvo">Active</option>
+                                        <option value="saab">Planned</option>
+                                        <option value="opel">Archived</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row pt-3">
+                            <div class="col">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">Project start</span>
+                                    </div>
+                                    <input name="projectStartTime" type="datetime-local" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row py-3">
+                            <div class="col">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">Deadline</span>
+                                    </div>
+                                    <input name="projectDeadline" type="datetime-local" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-12 pb-3">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">Planned working time</span>
+                                    </div>
+                                    <input class="form-control" type="number" placeholder="Hours">
+                                    <input class="form-control" type="number" min="0" max="59" placeholder="Mins">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <textarea name="projectNote" class="form-control" placeholder="Project note"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <div class="modal-footer">
+                    <div class="form-group">
+                        <input type="submit" value="Create new project" class="btn btn-outline-danger">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"
