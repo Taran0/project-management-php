f2d314452895b39d2a29e941625d427cd2a34bf1
Taran0 <49963209+Taran0@users.noreply.github.com>
2020-01-21 01:13:45 +0100

    task insert done

--- tasks.php
+++ tasks.php
@@ -1,7 +1,17 @@
 <?php
 require_once "include/db.php";
-$project_id = $_GET['id'];
-$project_name = $_GET['projectname'];
+
+if (isset($_GET['id']) && isset($_GET['projectname'])) {
+    $project_id = $_GET['id'];
+    $project_name = $_GET['projectname'];
+
+    setcookie("projectId", $project_id, time() + 3600);
+    setcookie("projectName", $project_name, time() + 3600);
+
+} else {
+    $project_id = $_COOKIE['projectId'];
+    $project_name = $_COOKIE['projectName'];
+}
 
 ?>
 
@@ -39,22 +49,62 @@ $project_name = $_GET['projectname'];
     </nav>
     <div class="container">
         <br>
-        <span class="m-0">Tasks of</span>
         <div class="row">
-        <h1 class="display-4 col d-inline"><?php echo $project_name; ?></h1>
-        <a class="text-secondary col text-right align-text-bottom pt-4" href="#">
-            <i class="fas fa-plus-square fa-3x"></i>
-        </a>
+            <span class="col">Tasks of</span>
+        </div>
+        <div class="row">
+            <h2 class="h3 col d-inline"><?php echo $project_name; ?></h2>
+            <!-- <h1 class="display-4 col d-inline"><?php echo $project_name; ?></h1> -->
+            <a class="col text-info text-right" href="" data-target="#newTaskModal" data-toggle="modal">
+                <i class="fas fa-plus-square fa-3x"></i>
+            </a>
 
-    </div>
-        
+        </div>
+    <?php
+if (isset($_POST['submit'])) {
+    if (!empty($_POST['taskName']) && !empty($_POST['taskStatus'])) {
+        $taskName = $_POST['taskName'];
+        $taskStatus = $_POST['taskStatus'];
+        $plannedWorkingTimeHours = $_POST['plannedWorkingTimeHours'];
+        $plannedWorkingTimeMinutes = $_POST['plannedWorkingTimeMinutes'];
+        $taskNote = $_POST['taskNote'];
+
+        //planned working time calculation
+        $plannedWorkingTime = intval($plannedWorkingTimeHours) * 3600 + intval($plannedWorkingTimeMinutes) * 60;
+
+        $connection;
+
+        $sql = "INSERT INTO tasks(task_name, task_status, task_expectedtime,
+            task_note, project_id)
+            VALUES(:tname, :tstat, :texptime, :tnote, :prid)";
+
+        //connectingDB objekt, metódusa a prepare
+        $stmt = $connection->prepare($sql);
+
+        //pdo-s value párosítása a változókkal
+        $stmt->bindValue(':tname', $taskName);
+        $stmt->bindValue(':tstat', $taskStatus);
+        $stmt->bindValue(':texptime', $plannedWorkingTime);
+        $stmt->bindValue(':tnote', $taskNote);
+        $stmt->bindValue(':prid', $project_id);
+
+        $execute = $stmt->execute();
+
+        if ($execute) {
+            echo "<span>Record has been added successfully</span>";
+        }
+
+    } else {
+        echo "<span>please add the data</span>";
+    }
+}
+?>
 
         <table class="table table-hover table-striped table-sm">
             <thead class="thead-dark">
                 <tr>
                     <th>No.</th>
                     <th>Task name</th>
-                    <th>Deadline</th>
                     <th>Task status</th>
                     <th>Do</th>
                     <th>Szerkeszt</th>
@@ -71,13 +121,11 @@ while ($row = $stmt->fetch()) {
 
     $id = $row['id'];
     $name = $row['task_name'];
-    $deadline = $row['task_deadline'];
     $status = $row['task_status'];
     ?>
                             <tr>
                                 <th class="align-middle" scope="row"><?php echo $i; ?></th>
                                 <td class="align-middle"><?php echo $name; ?></td>
-                                <td class="align-middle"><?php echo $deadline; ?></td>
                                 <td class="align-middle"><?php echo $status; ?></td>
                                 <td><a href="include/do.php?id=" class="btn btn-outline-danger btn-block btn-sm">Do</a></td>
                                 <td><a href="include/do.php?id=" class="btn btn-outline-primary btn-block btn-sm">Edit</a></td>
@@ -90,7 +138,68 @@ $i++;
             </tbody>
         </table>
 
+<!-- new project modal -->
+<div class="modal" id="newTaskModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                    <div class="modal-header bg-info">
+                      <h5 class="modal-title">Add new task to <?php echo $project_name; ?></h5>
+                      <button class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="tasks.php" method="post">
+                            <div class="row">
+                                <div class="col-md-7">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">Task name</span>
+                                        </div>
+                                        <input name="taskName" type="text" class="form-control" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-5">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">Status</span>
+                                        </div>
+                                        <select name="taskStatus" class="form-control">
+                                            <option value="inprogress">InProgress</option>
+                                            <option value="done">Done</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
 
+
+                            <div class="row pt-3">
+                                <div class="col-md-12 pb-3">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">Planned working time</span>
+                                        </div>
+                                        <input name="plannedWorkingTimeHours" class="form-control" type="number" placeholder="Hours" required>
+                                        <input name="plannedWorkingTimeMinutes" class="form-control" type="number" min="0" max="59" placeholder="Mins" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <textarea name="taskNote" class="form-control" placeholder="Task note"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <input type="submit" name="submit" value="Create new task" class="btn btn-outline-danger btn-block">
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="form-group">
+                            <button class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+            </div>
+        </div>
+        </div>
 
     </div>
 
